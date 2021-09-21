e80b361c78b8d85c5ad8eb254471ce2386e5bd4a
Thatsaphon <thatphon123@hotmail.com>
2021-09-21 20:00:17 +0700

    Add to Provider to Form

--- first_app/lib/pages/first_page.dart
+++ first_app/lib/pages/first_page.dart
@@ -1,6 +1,8 @@
+import 'package:first_app/models/first_form_model.dart';
 import 'package:first_app/pages/second_page.dart';
 import 'package:first_app/pages/third_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class FirstPage extends StatefulWidget {
   @override
@@ -39,12 +41,15 @@ class _FirstPageState extends State<FirstPage> {
           children: [
             Padding(
               padding: EdgeInsets.all(20),
-              child: Text('$_formData'),
+              child: Consumer<FirstFormModel>(builder: (context, form, child) {
+                return Text(
+                    '${form.firstName} ${form.lastName} is ${form.age} years old');
+              }),
             ),
             ElevatedButton(
               onPressed: () async {
                 var response = await Navigator.pushNamed(context, '/6');
-                if (response != null && !response.toString().isEmpty) {
+                if (response != null && response.toString().isNotEmpty) {
                   setState(() {
                     _formData = response.toString();
                   });
--- first_app/lib/pages/sixth_page.dart
+++ first_app/lib/pages/sixth_page.dart
@@ -1,4 +1,6 @@
+import 'package:first_app/models/first_form_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SixthPage extends StatelessWidget {
   @override
@@ -49,6 +51,7 @@ class _MyCustomFormState extends State<MyCustomForm> {
             onSaved: (value) {
               firstName = value!;
             },
+            initialValue: context.read<FirstFormModel>().firstName,
           ),
           TextFormField(
             validator: (value) {
@@ -65,36 +68,42 @@ class _MyCustomFormState extends State<MyCustomForm> {
             onSaved: (value) {
               lastName = value!;
             },
+            initialValue: context.read<FirstFormModel>().lastName,
           ),
           TextFormField(
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your age.';
-              }
-              if (int.parse(value) < 1) {
-                print(int.parse(value));
-                return 'Age must be a number and more than 0';
-              }
-              return null;
-            },
-            decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Enter your age",
-                icon: Icon(Icons.calendar_view_day)),
-            onSaved: (value) {
-              age = int.parse(value!);
-            },
-          ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your age.';
+                }
+                if (int.parse(value) < 1) {
+                  print(int.parse(value));
+                  return 'Age must be a number and more than 0';
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Enter your age",
+                  icon: Icon(Icons.calendar_view_day)),
+              onSaved: (value) {
+                age = int.parse(value!);
+              },
+              initialValue: context.read<FirstFormModel>().age.toString()),
           ElevatedButton(
               onPressed: () {
                 // ถ้า validate ผ่าน จะเข้าเงื่อนไขนี้
                 if (_formKey.currentState!.validate()) {
                   _formKey.currentState!.save();
+
+                  context.read<FirstFormModel>().firstName = firstName;
+                  context.read<FirstFormModel>().lastName = lastName;
+                  context.read<FirstFormModel>().age = age;
                   // ScaffoldMessenger.of(context)
                   //     .showSnackBar(SnackBar(content: Text(response)));
-                  var response =
-                      'Hooray!! $firstName $lastName is $age years old';
-                  Navigator.pop(context, response);
+                  // var response =
+                  //     'Hooray!! $firstName $lastName is $age years old';
+                  // Navigator.pop(context, response);
+                  Navigator.pop(context);
                 }
               },
               child: Text('Validate')),
