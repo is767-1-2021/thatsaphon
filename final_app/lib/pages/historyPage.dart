88a60249a291421d8a4b4f84bf225678ea2c9de4
Thatsaphon <thatphon123@hotmail.com>
2021-11-29 21:43:38 +0700

    Finally we made it on Tuesday

--- final_app/lib/pages/historyPage.dart
+++ final_app/lib/pages/historyPage.dart
@@ -10,43 +10,16 @@ class HistoryPage extends StatefulWidget {
 }
 
 class _HistoryPageState extends State<HistoryPage> {
-  List<HistoryModel> histories = [];
-  bool isLoading = false;
-
-  HistoryController controller = HistoryController();
-
-  // getHistories() async {
-  //   var tempHistories = await controller.fetchHistories();
-
-  //   setState(() {
-  //     histories = tempHistories;
-  //   });
-  // }
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   controller.onSync.listen((event) => setState(() => isLoading = event));
-  //   getHistories();
-  // }
-
   @override
   Widget build(BuildContext context) {
-    histories = context.read<HistoriesModel>().histories;
     return Scaffold(
       backgroundColor: Colors.blueGrey,
       appBar: AppBar(
         title: Text("History"),
-        actions: [
-          // Container(
-          //   child: Icon(Icons.calculate),
-          //   margin: EdgeInsets.only(right: 15),
-          // )
-        ],
       ),
       body: ListView.builder(
         padding: EdgeInsets.only(top: 20),
-        itemCount: histories.length,
+        itemCount: context.read<HistoriesModel>().histories.length,
         itemBuilder: (context, index) {
           return Container(
             padding: EdgeInsets.all(15),
@@ -57,7 +30,7 @@ class _HistoryPageState extends State<HistoryPage> {
                 Container(
                   margin: EdgeInsets.only(bottom: 5),
                   child: Text(
-                    histories[index].calculation,
+                    context.read<HistoriesModel>().histories[index].calculation,
                     style: TextStyle(
                         color: Colors.white,
                         fontSize: 18,
@@ -65,7 +38,7 @@ class _HistoryPageState extends State<HistoryPage> {
                   ),
                 ),
                 Text(
-                  histories[index].result,
+                  context.read<HistoriesModel>().histories[index].result,
                   style: TextStyle(
                       color: Colors.white,
                       fontSize: 32,
